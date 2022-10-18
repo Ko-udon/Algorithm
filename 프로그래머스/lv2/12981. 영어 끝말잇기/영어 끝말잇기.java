f30cd592ee923735f68123d52421255b2b41a71f
KoDongWoo <79897135+Ko-udon@users.noreply.github.com>
2022-10-18 11:01:32 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.22 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n, String[] words) {
+        int[] answer = new int[2];
+        List<String> list = new ArrayList<String>();
+        boolean flag = true;
+        
+        for(int i=0; i<words.length; i++){
+            if(list.contains(words[i])){   
+                answer[0] = (i%n) + 1;
+                answer[1] = (i/n) + 1;
+                flag = false;
+                break;
+            }
+            
+            list.add(words[i]); // 현재 단어 넣기
+            
+            // 전 단어의 끝과 지금 단어의 첫번째 단어가 다른경우
+            if(i>0 && words[i-1].charAt(words[i-1].length()-1) != words[i].charAt(0)){
+                answer[0] = (i%n) + 1;
+                answer[1] = (i/n) + 1;
+                flag = false;
+                break;
+            }
+        }
+        if(flag){
+        
+          return new int[]{0, 0};  
+        } 
+        return answer;
+    }
+}
