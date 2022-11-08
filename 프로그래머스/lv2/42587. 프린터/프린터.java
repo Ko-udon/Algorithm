f0f2d81ebae72ca4ed9d7b0ebd53cb0c1077deca
KoDongWoo <79897135+Ko-udon@users.noreply.github.com>
2022-11-08 13:08:03 +0900

    [level 2] Title: 프린터, Time: 0.57 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42587. 프린터/프린터.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        PriorityQueue<Integer> q = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i:priorities){
+            q.add(i);
+        }
+        
+        while (!q.isEmpty()) {
+            for (int i = 0; i < priorities.length; i++) {
+                if (priorities[i] == q.peek()) {
+                    if (i == location) {
+                        answer++;
+                        return answer;
+                    }
+                    q.poll();
+                    answer++;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
