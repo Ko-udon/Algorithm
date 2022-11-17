6411910fee551fe7a50aa3795273d35bf129fd71
KoDongWoo <79897135+Ko-udon@users.noreply.github.com>
2022-11-17 17:59:13 +0900

    [level 2] Title: 전화번호 목록, Time: 222.97 ms, Memory: 161 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -5,7 +5,7 @@ class Solution {
         Map<String,Integer> map=new HashMap<>();
         for(int i=0;i<phone_book.length;i++){
             map.put(phone_book[i],i);
-
+////
         }
 
         for(int i=0;i<phone_book.length;i++){
