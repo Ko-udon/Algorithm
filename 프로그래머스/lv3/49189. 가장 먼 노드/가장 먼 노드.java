cf8e368b44cfe0357377ca49343d2b7ab3908c69
KoDongWoo <79897135+Ko-udon@users.noreply.github.com>
2023-03-14 13:34:36 +0900

    [level 3] Title: 가장 먼 노드, Time: 29.48 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -7,42 +7,41 @@ class Solution {
     public int solution(int n, int[][] edge) {
         int answer = 0;
 
-        //간이 그래프 만들기
         ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
         for (int i = 0; i < n + 1; i++) {
             graph.add(new ArrayList<>());
         }
 
-        for (int i = 0; i < edge.length; i++) {
-            graph.get(edge[i][0]).add(edge[i][1]);
-            graph.get(edge[i][1]).add(edge[i][0]);
+        for (int[] ints : edge) {
+            graph.get(ints[0]).add(ints[1]);
+            graph.get(ints[1]).add(ints[0]);
         }
 
-        //방문 여부 확인
-        boolean[] visit = new boolean[n + 1];
-        visit[1] = true;
-
-        //거리 비교
-        int[] distance = new int[n + 1];
+        boolean[] visited = new boolean[n+1];
+        visited[1] = true;
 
         Queue<Integer> bfs = new LinkedList<>();
         bfs.add(1);
 
-        while (bfs.size() != 0) {
+        int[] distance = new int[n+1];
+
+        while (!bfs.isEmpty()) {
             int nowNode = bfs.poll();
             ArrayList<Integer> node = graph.get(nowNode);
-            for(int i = 0; i< node.size(); i++){
-                if(visit[node.get(i)] == false){
+            for (int i = 0; i < node.size(); i++) {
+                if (!visited[node.get(i)]) {
+                    visited[node.get(i)] = true;
                     bfs.add(node.get(i));
-                    visit[node.get(i)] = true;
                     distance[node.get(i)] = distance[nowNode] + 1;
                 }
             }
         }
+
         Arrays.sort(distance);
         int max = distance[n];
-        for(int i : distance){
-            if(i == max){
+
+        for(int i = 0; i<distance.length; i++){
+            if(distance[i] == max){
                 answer++;
             }
         }
