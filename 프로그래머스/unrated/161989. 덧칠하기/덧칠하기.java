34af197a363615346efeb7d231afa3c41d4f9c59
KoDongWoo <79897135+Ko-udon@users.noreply.github.com>
2023-03-15 14:33:59 +0900

    [unrated] Title: 덧칠하기, Time: 1.33 ms, Memory: 81.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        int paint = 0;
+        for (int i = 0; i < section.length; i++) {
+            int tmp = section[i];
+            if(tmp > paint){
+                answer++;
+                paint = tmp + m - 1;
+            }
+        }
+        return answer;
+    }
+}
