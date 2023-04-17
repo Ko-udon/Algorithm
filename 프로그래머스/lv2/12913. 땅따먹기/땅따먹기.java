92f4f94a2543acba75ee38bae47ba67681e39ad7
KoDongWoo <79897135+Ko-udon@users.noreply.github.com>
2023-04-18 00:19:23 +0900

    [level 2] Title: 땅따먹기, Time: 19.37 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.java
@@ -4,11 +4,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 class Solution {
     int solution(int[][] land) {
-        
 
         int answer = 0;
         for(int i=1;i<land.length;i++){
-            land[i][0]+=Math.max(land[i-1][1], Math.max(land[i-1][2],land[i-1][3]));
+            //각 행별 같은 열의 값을 제외하고 최대값에 더한값으로 업데이트 해준다.
+            land[i][0]+=Math.max(land[i-1][1], Math.max(land[i-1][2],land[i-1][3])); 
             land[i][1]+=Math.max(land[i-1][0], Math.max(land[i-1][2],land[i-1][3]));
             land[i][2]+=Math.max(land[i-1][0], Math.max(land[i-1][1],land[i-1][3]));
             land[i][3]+=Math.max(land[i-1][0], Math.max(land[i-1][1],land[i-1][2]));
