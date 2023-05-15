d4fc028caeedbd99e517a28d1e96300b00a8c379
KoDongWoo <79897135+Ko-udon@users.noreply.github.com>
2023-05-15 21:14:03 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.47 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -3,30 +3,21 @@ import java.util.*;
 class Solution {
     public int[] solution(int k, int[] score) {
         int[] result = new int[score.length];
-        
-        List<Integer> list = new ArrayList<>();
-        int min = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         
         for(int i = 0; i<score.length; i++){
             
-            if(list.size() < k){
-                list.add(score[i]);
-            } 
+            pq.add(score[i]);
             
-            else{
-                if(min < score[i]){
-                    list.remove(Integer.valueOf(min));
-                    list.add(score[i]);         
-                }
-                
+            if (pq.size() > k) {
+                pq.poll();
             }
+
+            result[i] = pq.peek();
             
-            min = Collections.min(list);
             
-            result[i] = min;
-            
-        
         }
+        
         return result;
     }
 }
