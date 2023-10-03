655cc3ccc3e1d5c3640ffd7d59e6b8364f091285
KoDongWoo <79897135+Ko-udon@users.noreply.github.com>
2023-10-03 15:32:13 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.35 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -0,0 +1,32 @@
+def solution(survey, choices):
+    # 좋은 답안
+    scores = {"A":0, "N":0, "C":0, "F":0, "M":0, "J":0, "R":0, "T":0}
+    
+    for idx, choice in enumerate(choices):
+        if choice - 4 > 0:
+            scores[survey[idx][1]] += choice - 4
+        elif choice - 4 < 0:
+            scores[survey[idx][0]] += 4 - choice
+
+    type = ""
+    if scores["R"] >= scores["T"]:
+        type += "R"
+    else:
+        type += "T"
+
+    if scores["C"] >= scores["F"]:
+        type += "C"
+    else:
+        type += "F"
+
+    if scores["J"] >= scores["M"]:
+        type += "J"
+    else:
+        type += "M"
+
+    if scores["A"] >= scores["N"]:
+        type += "A"
+    else:
+        type += "N"
+
+    return type
