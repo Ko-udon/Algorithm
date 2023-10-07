cb9971ef86d4e29f34ed038c842c713496c88992
KoDongWoo <79897135+Ko-udon@users.noreply.github.com>
2023-10-07 11:21:57 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.05 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,15 +1,8 @@
-def cnt_yak(number):
-        cnt = 1
-        for i in range(1, number//2+1):
-            if number % i == 0:
-                cnt += 1
-        return cnt
 def solution(left, right):
     answer = 0
-    for i in range(left, right+1):
-        cnt = cnt_yak(i)
-        if cnt % 2 == 0:
-            answer += i
-        else:
+    for i in range(left,right+1):
+        if int(i**0.5)==i**0.5:
             answer -= i
+        else:
+            answer += i
     return answer
