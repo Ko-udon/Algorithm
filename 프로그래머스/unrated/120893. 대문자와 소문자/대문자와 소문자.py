557394fb527a8d15b1d3ba0135564a80e5a75206
KoDongWoo <79897135+Ko-udon@users.noreply.github.com>
2023-10-19 17:29:58 +0900

    [level unrated] Title: 대문자와 소문자, Time: 0.31 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120893. 대문자와 소문자/대문자와 소문자.py
@@ -0,0 +1,10 @@
+import re
+
+def solution(my_string):
+    answer = ''
+    for s in my_string:
+        if s.isupper():
+            answer += s.lower()
+        else:
+            answer += s.upper()
+    return answer
