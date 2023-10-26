151c6fb0864c6688ef034caf87d4ad8e2e8b3dd7
KoDongWoo <79897135+Ko-udon@users.noreply.github.com>
2023-10-26 10:06:43 +0900

    [level unrated] Title: k의 개수, Time: 8.43 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120887. k의 개수/k의 개수.py
+++ 프로그래머스/unrated/120887. k의 개수/k의 개수.py
@@ -1,5 +1,2 @@
 def solution(i, j, k):
-    answer = 0
-    for s in range(i, j + 1):
-        answer += str(s).count(str(k))
-    return answer
+    return sum(str(s).count(str(k)) for s in range(i, j+1))
