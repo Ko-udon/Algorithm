87ed4c48b2d2b151ffeab6a050876f058f87c279
KoDongWoo <79897135+Ko-udon@users.noreply.github.com>
2023-10-28 23:11:17 +0900

    [level unrated] Title: 연속된 수의 합, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120923. 연속된 수의 합/연속된 수의 합.py
+++ 프로그래머스/unrated/120923. 연속된 수의 합/연속된 수의 합.py
@@ -1,6 +1,2 @@
 def solution(num, total):
-    tmp = 0
-    for i in range(1, num):
-        tmp += i
-    start = (total - tmp) // num
-    return [i for i in range(start, start + num)]
+    return [(total - (num * (num - 1) // 2)) // num + i for i in range(num)]
