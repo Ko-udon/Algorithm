81b49ef76a2e02e00f906c44f4f8f7f2564c12a8
KoDongWoo <79897135+Ko-udon@users.noreply.github.com>
2023-10-31 14:24:39 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/README.md
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/README.md
@@ -1,10 +1,10 @@
 # [level 2] 연속된 부분 수열의 합 - 178870 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178870?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178870) 
 
 ### 성능 요약
 
-메모리: 10 MB, 시간: 0.02 ms
+메모리: 10.3 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 2일 14:0:7
+2023년 10월 2일 14:24:39
 
 ### 문제 설명
 
--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
@@ -1,15 +1,20 @@
 def solution(sequence, k):
-
-    dic = {idx:num for idx,num in enumerate(sequence)}
     answer = []
-    start,sum = 0,0
-
-    for idx,item in enumerate(sequence):
-        end = idx
-        sum += item
-        while sum > k :
-            sum -= dic[start]
+    temp = sequence[0]
+    start,end = 0,0
+    while end <= len(sequence):
+        if temp < k:
+            if end + 1 == len(sequence):
+                break
+            end += 1
+            temp += sequence[end]
+        elif temp > k:
+            temp -= sequence[start]
             start += 1
-        if sum == k: answer.append([start,end])
-    answer.sort(key = lambda x: (x[1]-x[0],x[0]))
+        elif temp == k:
+            answer.append((start,end))
+            temp -= sequence[start]
+            start += 1
+    answer = sorted(answer,key=lambda x: x[1]-x[0])
+
     return answer[0]
