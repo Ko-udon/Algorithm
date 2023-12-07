6ea41f728f624a06f8df73f439a87aecc46a0109
KoDongWoo <79897135+Ko-udon@users.noreply.github.com>
2023-12-07 17:28:49 +0900

    [level unrated] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.11 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
+++ 프로그래머스/unrated/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
@@ -1,35 +1,9 @@
 def solution(data, ext, val_ext, sort_by):
     answer = []
-    
+    category = [ "code", "date", "maximum", "remain" ]
+
     for d in data:
-        if ext == 'code':
-            if val_ext > d[0]:
-                answer.append(d)
-        
-        elif ext == 'date':
-            if val_ext > d[1]:
-                answer.append(d)
-        
-        elif ext == 'maximum':
-            if val_ext > d[2]:
-                answer.append(d)
-                
-        elif ext == 'remain':
-            if val_ext > d[3]:
-                answer.append(d)
-                
-    
-    if sort_by == 'maximum':
-        answer.sort(key = lambda x:x[2])
-    
-    elif sort_by == 'remain':
-        answer.sort(key = lambda x:x[3])
-        
-    elif sort_by == 'code':
-        answer.sort(key = lambda x:x[0])
-        
-    elif sort_by == 'date':
-        answer.sort(key = lambda x:x[1])
-        
-    
-    return answer
+        if d[category.index(ext)] < val_ext:
+            answer.append(d)
+
+    return sorted(answer, key=lambda x: x[category.index(sort_by)])
