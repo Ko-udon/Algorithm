8213270fd2c4ae41674784319f3a84a09a61b982
KoDongWoo <79897135+Ko-udon@users.noreply.github.com>
2024-01-05 12:36:37 +0900

    [level unrated] Title: 문자열 섞기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.py
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.py
@@ -1,6 +1,2 @@
 def solution(str1, str2):
-    l = len(str1)
-    result = ''
-    for i in range(0, l):
-        result = result + (str1[i] + str2[i])
-    return result
+    return ''.join(str1[i] + str2[i] for i in range(0, len(str1)))
