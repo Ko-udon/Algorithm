37e47c3ddc27bafdf9e50e2baed1bf04f1fc1e0f
KoDongWoo <79897135+Ko-udon@users.noreply.github.com>
2024-01-12 21:52:46 +0900

    [level Lv.0] Title: 접미사 배열, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181909. 접미사 배열/접미사 배열.py
+++ 프로그래머스/Lv.0/181909. 접미사 배열/접미사 배열.py
@@ -1,5 +1,2 @@
 def solution(my_string):
-    answer = []
-    for i in range(1, len(my_string) + 1):
-        answer.append(my_string[-i:])
-    return sorted(answer)
+    return sorted(my_string[-i:] for i in range(1, len(my_string) + 1))
