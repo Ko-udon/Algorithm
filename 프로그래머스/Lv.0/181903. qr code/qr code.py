b5d27750e7ce4c523c9450448f13d5b947d404af
KoDongWoo <79897135+Ko-udon@users.noreply.github.com>
2024-01-13 01:26:47 +0900

    [level Lv.0] Title: qr code, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181903. qr code/qr code.py
+++ 프로그래머스/Lv.0/181903. qr code/qr code.py
@@ -1,6 +1,2 @@
 def solution(q, r, code):
-    arr = []
-    for i in range(0, len(code)):
-        if i%q == r:
-            arr.append(i)
-    return ''.join(code[i] for i in arr)
+    return code[r::q]
