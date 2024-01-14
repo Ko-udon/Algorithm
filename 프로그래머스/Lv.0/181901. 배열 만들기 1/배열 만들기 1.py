e71604765802f9a6eb0bb9c7c6231eaef01db914
KoDongWoo <79897135+Ko-udon@users.noreply.github.com>
2024-01-14 13:17:39 +0900

    [level Lv.0] Title: 배열 만들기 1, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181901. 배열 만들기 1/배열 만들기 1.py
+++ 프로그래머스/Lv.0/181901. 배열 만들기 1/배열 만들기 1.py
@@ -1,10 +1,2 @@
 def solution(n, k):
-    value = k
-    num = 1
-    answer = []
-    while value <= n:
-        answer.append(value)
-        num += 1
-        value = num * k
-    
-    return answer
+    return [i for i in range(k,n+1,k)]
