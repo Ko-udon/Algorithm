055f3da4141edb8e3bfb0652295edfa8c7ac2d59
KoDongWoo <79897135+Ko-udon@users.noreply.github.com>
2024-01-17 13:12:41 +0900

    [level 0] Title: 할 일 목록, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
@@ -1,6 +1,2 @@
 def solution(todo_list, finished):
-    result = []
-    for i in range(0, len(finished)):
-        if finished[i] == False:
-            result.append(todo_list[i])
-    return result
+    return [todo_list[i] for i in range(len(todo_list)) if finished[i] == False]
