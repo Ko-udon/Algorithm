dc07ce645dd4ffefb7e089c09d9644108aca82a7
KoDongWoo <79897135+Ko-udon@users.noreply.github.com>
2024-02-22 17:31:42 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -0,0 +1,24 @@
+def solution(progresses, speeds):
+    answer = []
+    arr = list(zip(progresses, speeds))
+    finish_day = []
+    for p in arr:
+        finish_day.append(checkFinishDay(p[0], p[1]))
+    #print(finish_day)
+    day = finish_day[0]
+    finish = 1
+    for i in range(1, len(finish_day)):
+        if day >= finish_day[i]:
+            finish += 1
+        else:
+            answer.append(finish)
+            day = finish_day[i]
+            finish = 1
+    answer.append(finish)
+    return answer
+def checkFinishDay(progress, speed):
+    day = 0
+    while progress < 100:
+        progress += speed
+        day += 1
+    return day
