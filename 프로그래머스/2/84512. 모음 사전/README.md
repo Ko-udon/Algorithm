ff28f4115e87bb8de599f9679356ba09d3f04625
KoDongWoo <79897135+Ko-udon@users.noreply.github.com>
2024-03-02 12:24:53 +0900

    [level 2] Title: 모음 사전, Time: 236.75 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/README.md
+++ 프로그래머스/2/84512. 모음 사전/README.md
@@ -1,10 +1,10 @@
 # [level 2] 모음 사전 - 84512 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.01 ms
+메모리: 10.3 MB, 시간: 236.75 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 01일 03:19:32
+2024년 03월 02일 12:24:51
 
 ### 문제 설명
 
--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,5 +1,14 @@
 def solution(word):
     answer = 0
-    for i, n in enumerate(word):
-        answer += (5 ** (5 - i) - 1) / (5 - 1) * "AEIOU".index(n) + 1
+    alpha  = ["A","E","I","O","U",""]
+    ans = []
+    for i in alpha:
+        for j in alpha:
+            for k in alpha:
+                for l in alpha:
+                    for m in alpha:
+                        w = i+j+k+l+m
+                        if w not in ans: ans.append(w)
+    ans.sort()
+    answer = ans.index(word)
     return answer
