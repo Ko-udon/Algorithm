b7926f47eb8e1dee3e62a7849f263debde1e8220
KoDongWoo <79897135+Ko-udon@users.noreply.github.com>
2024-03-11 19:38:05 +0900

    [level 2] Title: 택배상자, Time: 307.73 ms, Memory: 53 MB -BaekjoonHub

---
+++ 프로그래머스/2/131704. 택배상자/택배상자.py
@@ -0,0 +1,9 @@
+def solution(order):
+    answer = 0
+    stack = []
+    for i, o in enumerate(order):
+        stack.append(i+1)
+        while stack and stack[-1] == order[answer]:
+            stack.pop()
+            answer +=1
+    return answer
