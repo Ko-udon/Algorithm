7b0b7caf7fdcef6eddefc2f2798bd207adbf39e3
KoDongWoo <79897135+Ko-udon@users.noreply.github.com>
2024-04-03 11:06:01 +0900

    [level 2] Title: [3차] 방금그곡, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.py
@@ -0,0 +1,16 @@
+def solution(m, musicinfos): # split, replace 많이 쓰는 문제
+    answer = '(None)'
+    m = m.replace('A#', 'H').replace('C#', 'I').replace('D#', 'J').replace('F#', 'K').replace('G#', 'L')
+    max_time = 0
+    for info in musicinfos:
+        start, end, title, melody = info.split(',')
+        start = start.split(':')
+        end = end.split(':')
+        time = (int(end[0]) - int(start[0])) * 60 + int(end[1]) - int(start[1])
+        
+        melody = melody.replace('A#', 'H').replace('C#', 'I').replace('D#', 'J').replace('F#', 'K').replace('G#', 'L').replace('B#', 'B') # 문제 조건에 B#은 없다... 문제 오류인듯 
+        melody = melody * (time // len(melody)) + melody[0:time % len(melody)]
+        if m in melody and time > max_time:
+            max_time = time
+            answer = title
+    return answer
