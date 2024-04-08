c07a857754ed105d6eb1b9cd5973da9137a27194
KoDongWoo <79897135+Ko-udon@users.noreply.github.com>
2024-04-08 11:39:52 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.32 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
@@ -1,36 +1,14 @@
-from itertools import permutations as perm
-from collections import deque
-# https://2hs-rti.tistory.com/entry/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-Lv2-%EC%88%98%EC%8B%9D-%EC%B5%9C%EB%8C%80%ED%99%94-%ED%8C%8C%EC%9D%B4%EC%8D%AC 참조..
-
+# 깔끔한 풀이법 대단하다 ㄷㄷ..
 def solution(expression):
-    answer = 0
-    for priority in list(perm(['+', '-', '*'], 3)):
-        answer = max(answer, abs(make_result(priority, expression)))
-    return answer
-
-
-def make_result(priority, expression):
-    # arr 만들기
-    arr = deque()
-    num = ''
-    for k in expression:
-        if k.isdigit():
-            num += k
-        else:
-            arr.append(num)
-            num = ''
-            arr.append(k)
-    arr.append(num)
-    # 계산
-    for op in priority:
-        stack = []
-        while len(arr) != 0:
-            temp = arr.popleft()
-            if temp == op:
-                result = str(eval(stack.pop()+op+arr.popleft()))
-                stack.append(result)
-            else:
-                stack.append(temp)
-        arr = deque(stack)
-    return int(arr.pop())
-# 레벨 2 맞나?..
+    operations = [('+', '-', '*'),('+', '*', '-'),('-', '+', '*'),('-', '*', '+'),('*', '+', '-'),('*', '-', '+')]
+    answer = []
+    for op in operations:
+        a = op[0]
+        b = op[1]
+        temp_list = []
+        for e in expression.split(a):
+            temp = [f"({i})" for i in e.split(b)]
+            temp_list.append(f'({b.join(temp)})')
+        # print(temp_list)
+        answer.append(abs(eval(a.join(temp_list)))) # eval은 수식으로 구성된 문자열을 연산해줌
+    return max(answer)
