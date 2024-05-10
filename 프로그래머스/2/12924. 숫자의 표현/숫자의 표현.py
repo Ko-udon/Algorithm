b9de9dd08288f615c743a29ed539102a6f34b3e7
KoDongWoo <79897135+Ko-udon@users.noreply.github.com>
2024-05-10 11:24:45 +0900

    [level 2] Title: 숫자의 표현, Time: 3.45 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,12 +1,12 @@
 def solution(n):
     answer = 0
-    v = 1
-    for i in range(1, n+1):
-        value = 0
-        for v in range(i, n+1):
-            value += v
-            if value == n:
+    for i in range(1, n//2 + 1):
+        total = 0
+        for j in range(i, n+1):
+            total += j
+            if total == n:
                 answer += 1
-            elif value > n:
                 break
-    return answer
+            elif total > n:
+                break
+    return answer + 1
