fb69d205d844253909f6a6585d7f8e85de6df64b
KoDongWoo <79897135+Ko-udon@users.noreply.github.com>
2024-05-30 07:02:22 +0900

    [level 2] Title: 프로세스, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/README.md
+++ 프로그래머스/2/42587. 프로세스/README.md
@@ -1,10 +1,10 @@
 # [level 2] 프로세스 - 42587 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.12 ms
+메모리: 10.2 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 02월 25일 21:40:26
+2024년 05월 30일 07:02:20
 
 ### 문제 설명
 
--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,17 +1,11 @@
-from collections import deque
-
 def solution(priorities, location):
+    queue =  [(i,p) for i,p in enumerate(priorities)]
     answer = 0
-    # deque 생성
-    dq = deque([(v,i) for i,v in enumerate(priorities)])
-
-    while len(dq):
-        item = dq.popleft()
-        # pop한 아이템이 우선순위가 낮은 경우 다시 큐에 추가
-        if dq and max(dq)[0] > item[0]:
-            dq.append(item)
+    while True:
+        cur = queue.pop(0)
+        if any(cur[1] < q[1] for q in queue):
+            queue.append(cur)
         else:
             answer += 1
-            if item[1] == location:
-                break
-    return answer
+            if cur[0] == location:
+                return answer
