26e13c5e145706776dbd3d2bdfe46a771cfe2a47
KoDongWoo <79897135+Ko-udon@users.noreply.github.com>
2024-06-06 07:10:20 +0900

    [level 2] Title: 귤 고르기, Time: 14.89 ms, Memory: 18 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/README.md
+++ 프로그래머스/2/138476. 귤 고르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 귤 고르기 - 138476 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476) 
 
 ### 성능 요약
 
-메모리: 21.4 MB, 시간: 37.71 ms
+메모리: 18 MB, 시간: 14.89 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 4일 13:28:53
+2024년 06월 06일 07:10:17
 
 ### 문제 설명
 
--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,13 +1,25 @@
-def solution(k, tangerine):
-    mandarin = {}
-    for i in tangerine:
-        mandarin[i] = mandarin.get(i, 0) + 1
-    mandarin = sorted(mandarin.items(), key = lambda i : i[1], reverse = True)
+# def solution(k, tangerine):
+#     mandarin = {}
+#     for i in tangerine:
+#         mandarin[i] = mandarin.get(i, 0) + 1
+#     mandarin = sorted(mandarin.items(), key = lambda i : i[1], reverse = True)
 
-    count, mandarin_type = 0, 0
-    for key, value in mandarin:
-        count += value
-        mandarin_type += 1
-        if count >= k:
-            return mandarin_type
-    return -1
+#     count, mandarin_type = 0, 0
+#     for value in mandarin:
+#         count += value[1]
+#         mandarin_type += 1
+#         if count >= k:
+#             return mandarin_type
+#     return -1
+
+from collections import Counter
+def solution(k, tangerine):
+    answer = 0
+    counter = Counter(tangerine)
+    counter = sorted(counter.values())
+    counts = 0
+    
+    while counts < k:
+        counts += counter.pop()
+        answer += 1
+    return answer
