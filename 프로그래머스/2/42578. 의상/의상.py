b083e82c873e3b8f14f58a760b65bf97721362bb
KoDongWoo <79897135+Ko-udon@users.noreply.github.com>
2024-06-07 08:48:40 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,8 +1,23 @@
-from functools import reduce
+# from functools import reduce
+
+# def solution(clothes):
+#     style_count = {}
+#     for _, type in clothes:
+#         style_count[type] = style_count.get(type, 0) + 1
+    
+#     return reduce(lambda x, y: x * (y + 1), style_count.values(), 1) - 1
 
 def solution(clothes):
-    style_count = {}
-    for _, type in clothes:
-        style_count[type] = style_count.get(type, 0) + 1
-    
-    return reduce(lambda x, y: x * (y + 1), style_count.values(), 1) - 1
+    clothes_type = {}
+
+    for c, t in clothes:
+        if t not in clothes_type:
+            clothes_type[t] = 2
+        else:
+            clothes_type[t] += 1
+
+    cnt = 1
+    for num in clothes_type.values():
+        cnt *= num
+
+    return cnt - 1
