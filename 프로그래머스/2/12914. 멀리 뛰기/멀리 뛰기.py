c1e8503700b2df2afb15335bf4cb54f892d00809
KoDongWoo <79897135+Ko-udon@users.noreply.github.com>
2024-06-08 13:31:53 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.47 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -3,7 +3,7 @@ def solution(n):
     arr = [0 for i in range(2001)]
     arr[1] = 1 # 1칸짜리 경우의수는 1개
     arr[2] = 2 # 2칸짜피 경우의수는 1칸, 1칸 또는 2칸 으로 2개
-    
+
     for i in range(3, 2001):
         arr[i] = (arr[i-1] + arr[i-2]) % 1234567
     return arr[n]
