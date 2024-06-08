b397317b483470ea2fe7037b01c797b682b04da6
KoDongWoo <79897135+Ko-udon@users.noreply.github.com>
2024-06-09 00:54:00 +0900

    [level 2] Title: 땅따먹기, Time: 205.03 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/README.md
+++ 프로그래머스/2/12913. 땅따먹기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 땅따먹기 - 12913 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12913?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12913) 
 
 ### 성능 요약
 
-메모리: 32.5 MB, 시간: 207.90 ms
+메모리: 32.3 MB, 시간: 205.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 06일 19:48:10
+2024년 06월 09일 00:53:58
 
 ### 문제 설명
 
--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -1,9 +1,17 @@
-def solution(land):
-    answer = 0
-    for i in range(1, len(land)):
-        land[i][0] += max(land[i-1][1], max(land[i-1][2], land[i-1][3]))
-        land[i][1] += max(land[i-1][0], max(land[i-1][2], land[i-1][3]))
-        land[i][2] += max(land[i-1][0], max(land[i-1][1], land[i-1][3]))
-        land[i][3] += max(land[i-1][0], max(land[i-1][1], land[i-1][2]))
+# def solution(land):
+#     answer = 0
+#     for i in range(1, len(land)):
+#         land[i][0] += max(land[i-1][1], max(land[i-1][2], land[i-1][3]))
+#         land[i][1] += max(land[i-1][0], max(land[i-1][2], land[i-1][3]))
+#         land[i][2] += max(land[i-1][0], max(land[i-1][1], land[i-1][3]))
+#         land[i][3] += max(land[i-1][0], max(land[i-1][1], land[i-1][2]))
     
+#     return max(land[-1])
+
+def solution(land):
+
+    for i in range(1, len(land)):
+        for j in range(len(land[0])):
+            land[i][j] = max(land[i -1][: j] + land[i - 1][j + 1:]) + land[i][j]
+
     return max(land[-1])
