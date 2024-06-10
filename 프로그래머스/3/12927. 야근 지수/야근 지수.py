1e6c313cb3206ee558be9c245b0dba61db05f508
KoDongWoo <79897135+Ko-udon@users.noreply.github.com>
2024-06-10 19:43:11 +0900

    [level 3] Title: 야근 지수, Time: 391.47 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.py
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.py
@@ -1,12 +1,15 @@
 import heapq
 def solution(n, works):
+    
     if sum(works) <= n:
         return 0
-    works = [-work for work in works]
-    heapq.heapify(works)
-    print(works)
-    for _ in range(n):
-        work = heapq.heappop(works) + 1
-        heapq.heappush(works, work)
     
-    return sum([w**2 for w in works])
+    works = [-i for i in works]
+    heapq.heapify(works)
+    
+    for i in range(n):
+        work = heapq.heappop(works)
+        work += 1
+        heapq.heappush(works, work)
+        
+    return sum([i**2 for i in works])
