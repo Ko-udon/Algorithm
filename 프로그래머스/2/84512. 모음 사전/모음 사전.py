6281a7a75f49a033dfd904a02de8783c22802db8
KoDongWoo <79897135+Ko-udon@users.noreply.github.com>
2024-06-12 09:14:21 +0900

    [level 2] Title: 모음 사전, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,14 +1,21 @@
+# def solution(word):
+#     answer = 0
+#     alpha  = ["A","E","I","O","U",""]
+#     ans = []
+#     for i in alpha:
+#         for j in alpha:
+#             for k in alpha:
+#                 for l in alpha:
+#                     for m in alpha:
+#                         w = i+j+k+l+m
+#                         if w not in ans: ans.append(w)
+#     ans.sort()
+#     answer = ans.index(word)
+#     return answer
+
 def solution(word):
     answer = 0
-    alpha  = ["A","E","I","O","U",""]
-    ans = []
-    for i in alpha:
-        for j in alpha:
-            for k in alpha:
-                for l in alpha:
-                    for m in alpha:
-                        w = i+j+k+l+m
-                        if w not in ans: ans.append(w)
-    ans.sort()
-    answer = ans.index(word)
+    for i, n in enumerate(word):
+        answer += (5 ** (5 - i) - 1) / (5 - 1) * "AEIOU".index(n) + 1
     return answer
+
