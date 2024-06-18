38daddacb58cca6ee06f0cf62dee180ef90427cd
KoDongWoo <79897135+Ko-udon@users.noreply.github.com>
2024-06-19 06:30:55 +0900

    [level 2] Title: 롤케이크 자르기, Time: 544.88 ms, Memory: 51.2 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,13 +1,31 @@
+# from collections import Counter
+# def solution(topping):
+#     dic = Counter(topping)
+#     set_dic = set()
+#     answer = 0
+#     for i in topping:
+#         dic[i] -= 1
+#         set_dic.add(i)
+#         if dic[i] == 0:
+#             dic.pop(i)
+#         if len(dic) == len(set_dic):
+#             answer += 1
+#     return answer
+
 from collections import Counter
+
 def solution(topping):
-    dic = Counter(topping)
-    set_dic = set()
     answer = 0
-    for i in topping:
-        dic[i] -= 1
-        set_dic.add(i)
-        if dic[i] == 0:
-            dic.pop(i)
-        if len(dic) == len(set_dic):
+    be_ = set()
+    af = Counter(topping)
+
+    for t in topping:
+        be_.add(t)
+        af[t] -= 1
+
+        if af[t] == 0:
+            del af[t]
+        if len(be_) == len(af):
             answer += 1
+
     return answer
