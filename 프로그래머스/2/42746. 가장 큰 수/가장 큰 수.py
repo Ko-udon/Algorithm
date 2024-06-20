475b805fb42a850a95f1daf252e9b43775cab25c
KoDongWoo <79897135+Ko-udon@users.noreply.github.com>
2024-06-20 12:40:37 +0900

    [level 2] Title: 가장 큰 수, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,8 +1,25 @@
+# def solution(numbers):
+#     numbers_str = list(map(str, numbers))
+#     print(numbers_str)
+    
+#     # 문자열 비교를 통해 정렬, 1000까지라서 x*3
+#     # 근데 이걸 생각해내기 쉽지 않을듯...
+#     numbers_str.sort(key=lambda x: x*3, reverse=True)
+#     print(numbers_str)
+    
+#     answer = ''.join(numbers_str)
+#     print(answer)
+#     return str(int(answer))
+
+import functools
+
+def comparator(a,b):
+    t1 = a+b
+    t2 = b+a
+    return (int(t1) > int(t2)) - (int(t1) < int(t2)) #  t1이 크다면 1  // t2가 크다면 -1  //  같으면 0
+
 def solution(numbers):
-    numbers_str = list(map(str, numbers))
-    
-    # 문자열 비교를 통해 정렬, 1000까지라서 x*3
-    numbers_str.sort(key=lambda x: x*3, reverse=True)
-    
-    answer = ''.join(numbers_str)
-    return str(int(answer))
+    n = [str(x) for x in numbers]
+    n = sorted(n, key=functools.cmp_to_key(comparator),reverse=True)
+    answer = str(int(''.join(n)))
+    return answer
