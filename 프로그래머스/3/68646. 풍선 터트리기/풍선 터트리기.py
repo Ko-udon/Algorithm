451148261456fd3622d4e6eb0568e73732e844d4
KoDongWoo <79897135+Ko-udon@users.noreply.github.com>
2024-07-03 07:41:09 +0900

    [level 3] Title: 풍선 터트리기, Time: 134.96 ms, Memory: 53.2 MB -BaekjoonHub

--- 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.py
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.py
@@ -1,30 +1,46 @@
 # https://dev-note-97.tistory.com/165
 # 머리가 안돌아간다 ㅎ,,
-def solution(a):
-    if len(a) == 1:
-        return 1
 
-    answer = 2   # 기본적으로 양쪽 끝은 끝까지 살아남을 수 있음
+# def solution(a):
+#     if len(a) == 1:
+#         return 1
+
+#     answer = 2   # 기본적으로 양쪽 끝은 끝까지 살아남을 수 있음
     
-    # 최솟값 쌓기 ----------------
-    l_min = [a[0]]     
-    r_min = [a[-1]]
-    for i in range(1, len(a)):
-        if a[i] < l_min[-1]:
-            l_min.append(a[i])
-        else:
-            l_min.append(l_min[-1])
-        if a[len(a) - 1 - i] < r_min[-1]:
-            r_min.append(a[len(a) - 1 - i])
-        else:
-            r_min.append(r_min[-1])
-    r_min.reverse()    
-    # -----------------
+#     # 최솟값 쌓기 ----------------
+#     l_min = [a[0]]     
+#     r_min = [a[-1]]
+#     for i in range(1, len(a)):
+#         if a[i] < l_min[-1]:
+#             l_min.append(a[i])
+#         else:
+#             l_min.append(l_min[-1])
+#         if a[len(a) - 1 - i] < r_min[-1]:
+#             r_min.append(a[len(a) - 1 - i])
+#         else:
+#             r_min.append(r_min[-1])
+#     r_min.reverse()    
+#     # -----------------
 
-	# 찾은 최솟값으로 비교 계산 -------------
-    for i in range(1, len(a) - 1):
-        if l_min[i - 1] > a[i] or r_min[i + 1] > a[i]:
-            answer += 1
-    # --------------------------------
+# 	# 찾은 최솟값으로 비교 계산 -------------
+#     for i in range(1, len(a) - 1):
+#         if l_min[i - 1] > a[i] or r_min[i + 1] > a[i]:
+#             answer += 1
+#     # --------------------------------
             
+#     return answer
+
+
+def solution(a):
+    answer = 1
+    M = min(a)
+    for _ in range(2):
+        m = a[0]
+        i = 1
+        while m != M:
+            if m >= a[i]:
+                m = a[i]
+                answer += 1
+            i += 1
+        a.reverse()
     return answer
