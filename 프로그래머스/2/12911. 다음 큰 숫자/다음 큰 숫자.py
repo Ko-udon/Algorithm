6da5989985214e21312fff482efcc4aa0e5015cf
KoDongWoo <79897135+Ko-udon@users.noreply.github.com>
2024-07-11 00:56:02 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,7 +1,7 @@
 def solution(n):
-    countOne = bin(n).count('1')
+    count_one = bin(n).count('1')
     while True:
         n = n + 1
-        if bin(n).count('1') == countOne:
+        if bin(n).count('1') == count_one:
             break
     return n
