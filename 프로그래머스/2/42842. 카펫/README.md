d901a5beb17a72f35e9add6f2da90ed030cabc13
KoDongWoo <79897135+Ko-udon@users.noreply.github.com>
2024-07-18 07:27:25 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.01 ms
+메모리: 10.1 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 02일 22:37:59
+2024년 07월 18일 07:27:23
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,6 +1,13 @@
+# def solution(brown, yellow):
+#     answer = []
+#     n = int(brown/2 + 2)
+#     for i in range(n-3, int(n/2 - 1), -1):
+#         if (i-2)*(n-i-2) == yellow:
+#             return [i,n-i]
+
+
+import math
 def solution(brown, yellow):
-    answer = []
-    n = int(brown/2 + 2)
-    for i in range(n-3, int(n/2 - 1), -1):
-        if (i-2)*(n-i-2) == yellow:
-            return [i,n-i]
+    w = ((brown+4)/2 + math.sqrt(((brown+4)/2)**2-4*(brown+yellow)))/2
+    h = ((brown+4)/2 - math.sqrt(((brown+4)/2)**2-4*(brown+yellow)))/2
+    return [w,h]
