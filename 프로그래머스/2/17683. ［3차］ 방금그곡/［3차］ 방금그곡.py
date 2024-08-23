ee932122fcb3cc30f460a0cf39433b6138f6a138
KoDongWoo <79897135+Ko-udon@users.noreply.github.com>
2024-08-23 09:45:54 +0900

    [level 2] Title: [3차] 방금그곡, Time: 1.20 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.py
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.py
@@ -1,6 +1,6 @@
 def solution(m, musicinfos): # split, replace 많이 쓰는 문제
     answer = '(None)'
-    m = m.replace('A#', 'H').replace('C#', 'I').replace('D#', 'J').replace('F#', 'K').replace('G#', 'L')
+    m = m.replace('A#', 'H').replace('C#', 'I').replace('D#', 'J').replace('F#', 'K').replace('G#', 'L').replace('B#', 'M') # 문제조건에 없는 B#...
     max_time = 0
     for info in musicinfos:
         start, end, title, melody = info.split(',')
@@ -8,7 +8,7 @@ def solution(m, musicinfos): # split, replace 많이 쓰는 문제
         end = end.split(':')
         time = (int(end[0]) - int(start[0])) * 60 + int(end[1]) - int(start[1])
         
-        melody = melody.replace('A#', 'H').replace('C#', 'I').replace('D#', 'J').replace('F#', 'K').replace('G#', 'L').replace('B#', 'B') # 문제 조건에 B#은 없다... 문제 오류인듯 
+        melody = melody.replace('A#', 'H').replace('C#', 'I').replace('D#', 'J').replace('F#', 'K').replace('G#', 'L').replace('B#', 'M') # 문제 조건에 B#은 없다... 문제 오류인듯 34번 테스트케이스 
         melody = melody * (time // len(melody)) + melody[0:time % len(melody)]
         if m in melody and time > max_time:
             max_time = time
