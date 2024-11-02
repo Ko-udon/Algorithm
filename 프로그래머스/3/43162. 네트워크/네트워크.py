c0aaa100ccd4f500031283690792c94d044c7e30
KoDongWoo <79897135+Ko-udon@users.noreply.github.com>
2024-11-02 23:40:31 +0900

    [level 3] Title: 네트워크, Time: 5.14 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,15 +1,11 @@
 def solution(n, computers):
-    def dfs(v):
-        visited[v] = True
-        for i in range(n):    # 인접노드 탐구 
-            if not visited[i] and computers[v][i]:    # unvisited + 인접할 때 
-                dfs(i)
-
-    count = 0   # 네트워크 수
-    visited = [False] * (n)   
-
-    for node in range(n):
-        if not visited[node]:
-            dfs(node)
-            count += 1
-    return count
+    temp = []
+    for i in range(n):
+        temp.append(i)
+    for i in range(n):
+        for j in range(n):
+            if computers[i][j]:
+                for k in range(n):
+                    if temp[k] == temp[i]:
+                        temp[k] = temp[j]
+    return len(set(temp))
